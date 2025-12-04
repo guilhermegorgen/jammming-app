2727ab9d0c8b1c798fd5bc0f67f2c74f216d9f4d
Guilherme <gorgen.gui@gmail.com>
2025-12-03 21:28:48 -0300

    vite config changes

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,22 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "/jammming-app/",
-})
+
+  esbuild: {
+    loader: 'jsx',
+  },
+
+  resolve: {
+    alias: {
+      '.runtimeConfig': 'runtimeConfig.browser'
+    },
+  },
+
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        'js': 'jsx',
+      }
+    }
+  }
+});
