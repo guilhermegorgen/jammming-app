e3e18a0941690f8c8e37871c10cfa44b08fd2992
Guilherme <gorgen.gui@gmail.com>
2025-12-10 21:34:36 -0300

    SPotify changes

--- src/Spotify.js
+++ src/Spotify.js
@@ -1,5 +1,5 @@
 const clientId = 'e27bcceb0b7643ea9fb07295db107f0e';
-const redirectUri = "http://10.0.0.187:5173/callback";
+const redirectUri = "https://127.0.0.1:8888/callback";
 let accessToken;
 
 const Spotify = {
